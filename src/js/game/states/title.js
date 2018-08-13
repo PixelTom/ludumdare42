7adfb4215721701eeab8ae45f23f42c3c3ff5a28
Tom Gattenhof <tom@pixeltom.net>
2018-08-13 19:51:46 +1000

    Title credits

--- src/js/game/states/title.js
+++ src/js/game/states/title.js
@@ -26,12 +26,42 @@ class Title extends Phaser.State {
     music.loop = true;
     music.play();
 
-    this.game.input.onDown.add(this.startGame, this);
-  }
+    let credits = null;
 
-  startGame() {
-    music.stop();
-    this.game.state.start('Game');
+    const startGame = function () {
+      music.stop();
+      this.game.state.start('Game');
+    };
+
+    const showCredits = function (phase) {
+      const textArray = [
+        'CODE, ART & SOUND\n\nGITHUB.COM/PIXELTOM\nGITHUB.COM/AKURN',
+        'ADDITIONAL ART\n\nGITHUB.COM/DEVELOPERALAN\nDEVIANTART.COM/ORTEIL\nCCRGEEK.WORDPRESS.COM',
+        'CLICK TO PLAY',
+      ];
+      if (credits != null) {
+        credits.destroy();
+      }
+      credits = this.game.add.text(this.game.world.centerX, this.game.world.height * 0.6, textArray[phase], {
+        font: 'normal 30px "Press Start 2P"',
+        fill: '#36ff90',
+        align: 'center',
+      });
+      credits.anchor.setTo(0.5, 0.5);
+      const tweenA = this.game.add.tween(credits).from({ alpha: 0 }, 500, 'Linear', true);
+      const tweenB = this.game.add.tween(credits).to({ alpha: 0 }, 500, 'Linear', false, 3700);
+
+      tweenA.chain(tweenB);
+
+      phase += 1;
+      if (phase > 2) {
+        phase = 0;
+      }
+      this.game.time.events.add(5000, showCredits, this, phase);
+    };
+
+    this.game.input.onDown.add(startGame, this);
+    this.game.time.events.add(2000, showCredits, this, 0);
   }
 }
 
